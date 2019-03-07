5c8dbfb4ab71e2201bcdeb1ce43e3cd70e9a12bd
Alikhan <alikhan.okas@gmail.com>
2019-03-08 04:16:32 +0600

    Added project

---
+++ Java Spring Boot Project/src/main/java/com/in28minutes/springboot/web/DatabaseConfiguration.java
@@ -0,0 +1,44 @@
+package com.in28minutes.springboot.web;
+
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+/**
+ * Spring configuration for database
+ */
+@org.springframework.context.annotation.Configuration
+
+
+public class DatabaseConfiguration {
+    private static final String DB = "todo";
+    private static final String HOST = "localhost";
+
+    public static final String USERNAME = "todo_admin";
+    public static final String PASSWORD = "saul";
+
+
+    @Bean
+    public DataSource dataSource() {
+        return getDataSrouce();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+    private static DataSource getDataSrouce() {
+        String url = String.format("jdbc:postgresql://%s/%s",HOST,DB);
+        return DataSourceBuilder
+                .create()
+                .username(USERNAME)
+                .password(PASSWORD)
+                .url(url)
+                .build();
+    }
+}
